09da27101971ecdeda7b66add3c867db948879b9
DarlanHSO <darlan.hs.oliveira@gmail.com>
2025-11-17 23:57:18 -0300

    pratica 4

--- src/main/java/com/example/Pratica4/DevOps/presentation/controllers/CursoController.java
+++ src/main/java/com/example/Pratica4/DevOps/presentation/controllers/CursoController.java
@@ -3,8 +3,11 @@ package com.example.Pratica4.DevOps.presentation.controllers;
 import com.example.Pratica4.DevOps.application.dtos.CursoDTO;
 import com.example.Pratica4.DevOps.application.services.ICursoAppService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,5 +32,11 @@ public class CursoController {
     @GetMapping("/{id}")
     public ResponseEntity<CursoDTO> getById(@PathVariable("id") String id) {
         return service.findById(id).map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<CursoDTO> create(@RequestBody CursoDTO dto) {
+        CursoDTO created = service.create(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
     }
 }
